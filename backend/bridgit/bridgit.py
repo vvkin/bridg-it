ba606a1b41f12fa6efb70bba9d2b64bcf8a9d5e2
Vadym Kinchur <vkincur@gmail.com>
2020-11-13 01:50:47 +0200

    Add grid creation and player move validation

--- backend/bridgit/bridgit.py
+++ backend/bridgit/bridgit.py
@@ -1,9 +1,32 @@
 from typing import Tuple
-from .const import levels
+from const import LEVELS, GRID_SIZE, CORNERS
+import numpy as np
 
 class Bridgit:
-    def __init__(self, level):
-        self.depth = levels[level]
-
+    def __init__(self, level: str, player_num: int):
+        self.depth = LEVELS[level]
+        self.grid = np.zeros((GRID_SIZE, GRID_SIZE), np.int)
+        self.player_num = player_num
+        self.init_grid()
+    
+    def init_grid(self):
+        for i in range(GRID_SIZE):
+            if (i % 2):
+                self.grid[i][::2] = 2
+            else:
+                self.grid[i][1::2] = 1
+    
+    def is_valid(self, move: Tuple) -> bool:
+        return (
+            self.grid[move] and move not in CORNERS
+                and not (move[0] in (0, GRID_SIZE - 1) and self.player_num != 1)
+                and not (move[1] in (0, GRID_SIZE - 1) and self.player_num != 2)
+        )
+        
     def handle_move(self, move: Tuple) -> Tuple:
+        self.grid[move] = self.player_num
+        self.make_move()
+    
+    def make_move(self):
+        # find optimal move here
         pass
--- backend/bridgit/const.py
+++ backend/bridgit/const.py
@@ -1,5 +1,14 @@
-levels = {
+LEVELS = {
     'low': 3,
     'medium': 6,
     'hight': 12
 }
+
+GRID_SIZE = 11 # 11 * 11 grid
+
+CORNERS = (
+    (0, 0),
+    (0, GRID_SIZE - 1),
+    (GRID_SIZE - 1, 0),
+    (GRID_SIZE - 1, GRID_SIZE - 1)
+)
